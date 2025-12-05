d4c9c176ffa7e84baa9e722425f77a9cf62a307d
Dsssyc <dsssyc02@gmail.com>
2025-12-05 05:59:42 +0000

    Refactor run.py: clean up unused gRPC code and adjust is_ready() call placement

--- python/snaking/run.py
+++ python/snaking/run.py
@@ -4,29 +4,10 @@ import grpc
 import struct
 import os
 import random
-
-# from src.proto import snaking_pb2 as pb
-# from src.proto import snaking_pb2_grpc as pb_grpc
-
-# SERVER_ADDRESS = 'unix:///tmp/controller.sock'
-
-# channel = grpc.insecure_channel(SERVER_ADDRESS)
-# stub = pb_grpc.ControllerStub(channel)
-
-# while True:
-#     try:
-#         response = stub.WaitForStart(pb.Empty())
-#         if response.ready:
-#             print("Received start signal from controller.")
-#             break
-#     except grpc.RpcError as e:
-#         if e.code() == grpc.StatusCode.UNAVAILABLE:
-#             print("Controller not available, retrying...")
-#             time.sleep(1)
-#         else:
-#             print(f"gRPC error: {e}")
-#             sys.exit(1)
+import fastdb4py as fdb
 
 from src.snaking import is_ready, wait_for_sync
 
-is_ready()
+if __name__ == '__main__':
+    is_ready()
+    
